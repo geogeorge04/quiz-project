97aa375fe30134c843002eacb9254dc2cdd8e5a1
GEO GEORGE <geogeorge04@gmail.com>
2025-05-15 13:18:15 +0530

    Update MongoDB connection string

---
+++ server/db.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI || "mongodb+srv://geogeorge04:@quiz.rs7jx0m.mongodb.net/?retryWrites=true&w=majority&appName=Quiz";
+const client = new MongoClient(uri);
+
+let db;
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        db = client.db('quiz-app');
+        return db;
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        throw error;
+    }
+}
+
+function getDb() {
+    if (!db) {
+        throw new Error('Database not initialized. Call connectToDatabase first.');
+    }
+    return db;
+}
+
+process.on('SIGINT', async () => {
+    if (client) {
+        await client.close();
+        console.log('MongoDB connection closed');
+    }
+    process.exit(0);
+});
+
+module.exports = {
+    connectToDatabase,
+    getDb
+}; 
